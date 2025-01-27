984861499254218069f5a150ba0e86d02608414d
Poukisans <al.andr@hotmail.fr>
2025-01-27 01:38:17 +0100

    add config

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// Définir des constantes ou variables globales
+define('SITE_NAME', 'Trello');
+define('BASE_URL', "/trello/");
+define('DB_HOST', "localhost");
+define('DB_NAME', "and_trello");
+define('DB_USER', "root");
+define('DB_PASS', "");
+?>
